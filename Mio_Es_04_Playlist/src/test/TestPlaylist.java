318d100e5515c111b3fa3e97ac2313b926412fa1
nxingram <nxingram@users.noreply.github.com>
2021-05-29 14:16:56 +0200

    aggiornato test playlist

--- Mio_Es_04_Playlist/src/test/TestPlaylist.java
+++ Mio_Es_04_Playlist/src/test/TestPlaylist.java
@@ -13,24 +13,24 @@ public class TestPlaylist {
 		ClipVideo cv2 = new ClipVideo("video2", 4, 25);
 		ClipVideo cv3 = new ClipVideo("video4", 35, 45);
 		
-		System.out.println("aggiunto: " + pl.aggiungiVideo(cv1));
-		System.out.println("aggiunto: " + pl.aggiungiVideo(cv2));
+		System.out.println("aggiunto (true): " + pl.aggiungiVideo(cv1));
+		System.out.println("aggiunto (true): " + pl.aggiungiVideo(cv2));
 		
 		// deve restituire false, troppo lungo
-		System.out.println("aggiunto lungo: " + pl.aggiungiVideo(cv3));
+		System.out.println("aggiunto lungo (false): " + pl.aggiungiVideo(cv3));
 		
 		// 2 vid, quello lungo non ci sta
-		System.out.println("nClipVid: " + pl.numeroClipVideo());
+		System.out.println("nClipVid (2): " + pl.numeroClipVideo());
 
 		// titolo duplicato, deve rest false
-		System.out.println("duplicati: " + pl.verificaDuplicati());
+		System.out.println("duplicati (false): " + pl.verificaDuplicati());
 		
 		// aggiungo video con titolo duplicato, rest true
 		ClipVideo cv4 = new ClipVideo("video1", 2, 45);
-		System.out.println("aggiunto duplicato: " + pl.aggiungiVideo(cv4));
+		System.out.println("aggiunto duplicato (true): " + pl.aggiungiVideo(cv4));
 		
 		// titolo duplicato, deve rest true
-		System.out.println("duplicati: " + pl.verificaDuplicati());
+		System.out.println("duplicati (true): " + pl.verificaDuplicati());
 	}
 
 }
